0d11b320052cf984bbe864f1875a058739c98654
sasha44 <12345@i.ua>
2018-03-28 19:22:12 +0300

    obj created name vasya

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
 
+spl_autoload_register(function ($class){
+     $classFileName = 'vendor' . DIRECTORY_SEPARATOR . $class . '.php';
+    if (file_exists($classFileName)) {
+        include_once $classFileName;
+        return TRUE;
+    }
+    return FALSE;
+});
+
+$testObj= new Test();
+$testObj->name = 'vasya';
+var_dump($testObj);
+echo ($testObj->name);
