87c7cad4d6508666a4fcc484cdd4279c55c23e44
sasha44 <12345@i.ua>
2018-03-28 20:18:46 +0300

    student obj

--- index.php
+++ index.php
@@ -16,3 +16,6 @@ spl_autoload_register(function ($class){
 
 $human = new Human('vasya', 'pupkin', 22, 'troickaya');
 echo $human;
+
+$student  =  new Student ('kolya','vasin',33,'bankovskaya'); 
+echo $student;
